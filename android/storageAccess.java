2818780133ee0a5a8de14a638a52b7f9226a33f9
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2022-01-12 13:08:34 +0530

    file access android 11

---
+++ android/storageAccess.java
@@ -0,0 +1,19 @@
+// on main activity onCreate()
+
+        if (Build.VERSION.SDK_INT >= 30){
+            if (!Environment.isExternalStorageManager()){
+                Intent getpermission = new Intent();
+                getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivity(getpermission);
+            }
+        }
+		
+// https://stackoverflow.com/questions/66839849/android-studio-getting-error-ioexception-operation-not-permitted-while-creating
+
+//use legacy to get access in android 10
+
+android:requestLegacyExternalStorage='true'
+
+//use legacy to get access in android 11 should work but didn't
+
+android:preserveLegacyExternalStorage='true'
