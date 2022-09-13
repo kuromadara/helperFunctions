1402e5c07d372bb3acb6ca85bfdd94d13c9ed9ad
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2022-09-13 12:08:42 +0530

    remove unsafe tags and attributes

---
+++ miscellaneous/removeUnsafeTags.php
@@ -0,0 +1,36 @@
+function strip_unsafe($string, $img=false)
+{
+    // Unsafe HTML tags that members may abuse
+    $unsafe=array(
+    '/<iframe(.*?)<\/iframe>/is',
+    '/<title(.*?)<\/title>/is',
+    '/<pre(.*?)<\/pre>/is',
+    '/<frame(.*?)<\/frame>/is',
+    '/<frameset(.*?)<\/frameset>/is',
+    '/<object(.*?)<\/object>/is',
+    '/<script(.*?)<\/script>/is',
+    '/<embed(.*?)<\/embed>/is',
+    '/<applet(.*?)<\/applet>/is',
+    '/<meta(.*?)>/is',
+    '/<!doctype(.*?)>/is',
+    '/<link(.*?)>/is',
+    '/<body(.*?)>/is',
+    '/<\/body>/is',
+    '/<head(.*?)>/is',
+    '/<\/head>/is',
+    '/onload="(.*?)"/is',
+    '/onunload="(.*?)"/is',
+    '/<html(.*?)>/is',
+    '/<\/html>/is');
+
+    // Remove graphic too if the user wants
+    if ($img==true)
+    {
+        $unsafe[]='/<img(.*?)>/is';
+    }
+
+    // Remove these tags and all parameters within them
+    $string=preg_replace($unsafe, "", $string);
+
+    return $string;
+}
