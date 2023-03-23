b812d847a2a1b033a66b54adb4702830b3035f4d
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2023-03-23 12:33:53 +0530

    sampling rate added

--- flutter/flutterPhysics/code.dart
+++ flutter/flutterPhysics/code.dart
@@ -13,14 +13,23 @@ AccelerometerEvent _accelerometerEvent = AccelerometerEvent(0, 0, 0);
           double y = _accelerometerEvent.y;
           double z = _accelerometerEvent.z;
 
-          print("x: ${x}");
-          print("y: ${y}");
-          print("z: ${z}");
-
           double acceleration = sqrt(x * x + y * y + z * z) -
               _gravitational_acceleration_constant;
           print("acceleration: ${acceleration}");
           _thresholdGravity = max(acceleration, _thresholdGravity);
+
+          if (_samplingRate == null) {
+            // Start timer when first event is received
+            Timer.periodic(Duration(milliseconds: 1), (timer) {
+              if (_accelerometerEvent != null) {
+                // Stop timer when second event is received
+                timer.cancel();
+                double elapsedMs = timer.tick * 1.0;
+                _samplingRate = 1000.0 / elapsedMs;
+                print("Sampling rate: $_samplingRate Hz");
+              }
+            });
+          }
         }
 
         print("accelerometerEvent: ${_accelerometerEvent}");
