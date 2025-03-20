acdbcb69902f66a9b35eac7a5f5da4f77ebce9ff
kuromadara <dowarah.dayananda35@gmail.com>
2025-03-20 13:09:52 +0530

    mpdf assamese fix in table header

---
+++ codeigniter/mdf_indic_lang_fix.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="as">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style>
+        body {
+            font-family: freesans;
+            margin: 20px;
+            padding: 20px;
+            border: 1px solid #000;
+        }
+
+
+        th,
+        td {
+            font-weight: normal; /* Disable bold if font lacks bold support */
+            font-family: 'freesans'; /* Force Assamese support */
+            border: 1px solid black;
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
+    <title>অসম চৰকাৰ নোটিশ</title>
+</head>
